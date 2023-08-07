870b7b3628119c6ca078312df2b840485e8d2ba3
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-08-08 04:08:04 +0530

    add

---
+++ Frontend_UI/src/Pages/ForgetPassword.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function ForgetPassword() {
+  const [pass, setPass] = useState("");
+  const { id, token } = useParams();
+
+  const navigate = useNavigate();
+
+  const PasswordUpdateFunc = async () => {
+    try {
+      const obj = { password: pass }; // Replace with the actual password
+
+      await fetch(`http://localhost:8080/users/forgetpassword/${id}/${token}`, {
+        method: "PATCH",
+        body: JSON.stringify(obj),
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.success == true) {
+            alert(res.message);
+            navigate("/login");
+          }
+          if (res.success == false) {
+            alert(res.message);
+          }
+          console.log(res);
+        }); // Corrected typo here
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>ForgetPassword</h1>
+      <div className="Input_forget_pass">
+        <input
+          type="password"
+          placeholder="New Password"
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <button onClick={PasswordUpdateFunc}>Submit</button>
+      </div>
+    </div>
+  );
+}
